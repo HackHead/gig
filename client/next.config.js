8baac363faeeacfaef851ae81cd475addcf2e41b
Andrew <SolomonAndreew@tutanota.com>
2023-05-14 15:54:01 +0300

    feat: saving intermidiate results

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    i18n: {
+        /**
+         * Provide the locales you want to support in your application
+         */
+        locales: ["en", "ru", "ua"],
+        /**
+         * This is the default locale you want to be used when visiting
+         * a non-locale prefixed path.
+         */
+        defaultLocale: "en",
+        localeDetection: true
+    },
+}
 
 module.exports = nextConfig
