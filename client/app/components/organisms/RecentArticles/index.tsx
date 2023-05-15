321d2a01ad512a1104f60ce24700930aa7ea14e2
Andrew <SolomonAndreew@tutanota.com>
2023-05-15 18:51:24 +0300

    feat: create recent article component

---
+++ client/app/components/organisms/RecentArticles/index.tsx
@@ -0,0 +1,128 @@
+const RecentArticles = () => {
+  return (
+    <section id="blog" className="max-w-7xl mx-auto my-20">
+      <div className=" box-screen pt-20">
+        <div className=" text-center mb-16 max-w-[500px] mx-auto">
+          <div className="animate-fade-in-up delay-200">
+            <h2 className="  text-main font-bold font-khand text-3xl sm:text-4xl mb-4">
+              Recent Articles
+            </h2>
+            <p className=" text-grey">
+              There are many variations of passages of Lorem Ipsum available but
+              the majority have suffered alteration in some form.
+            </p>
+          </div>
+        </div>
+        <div className=" grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="col-auto lg:col-start-2 lg:row-start-1 lg:row-end-3">
+            <div className="animate-fade-in-up delay-200">
+              <div className=" overflow-hidden rounded-xl relative">
+                <img
+                  src="https://appwind.tuanha.dev/images/blog-3.jpg"
+                  alt="blog"
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute right-0 top-0 flex items-end w-full h-full bg-gradient-to-t from-[#07102d] to-transparent">
+                  <div className=" flex justify-between items-end p-8 w-full ">
+                    <div className="max-w-[400px]">
+                      <p className=" mb-2 text-white font-bold font-khand text-2xl cursor-pointer hover:text-primary">
+                        50+ High rated online banking mobile apps on playstore.
+                      </p>
+                      <p className=" text-white text-base">
+                        Continue reading...
+                      </p>
+                    </div>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 448 512"
+                      className=" text-white hover:text-primary text-xl cursor-pointer min-w-[16px]"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="animate-fade-in-up delay-200">
+              <div className=" overflow-hidden rounded-xl relative">
+                <img
+                  src="https://appwind.tuanha.dev/images/blog-1.jpg"
+                  alt="blog"
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute right-0 top-0 flex items-end w-full h-full bg-gradient-to-t from-[#07102d] to-transparent">
+                  <div className=" flex justify-between items-end p-8 w-full ">
+                    <div className="max-w-[400px]">
+                      <p className=" mb-2 text-white font-bold font-khand text-2xl cursor-pointer hover:text-primary">
+                        Want to use Appwind in your organization?
+                      </p>
+                      <p className=" text-white text-base">
+                        Continue reading...
+                      </p>
+                    </div>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 448 512"
+                      className=" text-white hover:text-primary text-xl cursor-pointer min-w-[16px]"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="animate-fade-in-up delay-200">
+              <div className=" overflow-hidden rounded-xl relative">
+                <img
+                  src="https://appwind.tuanha.dev/images/blog-2.jpg"
+                  alt="blog"
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute right-0 top-0 flex items-end w-full h-full bg-gradient-to-t from-[#07102d] to-transparent">
+                  <div className=" flex justify-between items-end p-8 w-full ">
+                    <div className="max-w-[400px]">
+                      <p className=" mb-2 text-white font-bold font-khand text-2xl cursor-pointer hover:text-primary">
+                        Interested in selling your own Apps on marketplace?
+                      </p>
+                      <p className=" text-white text-base">
+                        Continue reading...
+                      </p>
+                    </div>
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 448 512"
+                      className=" text-white hover:text-primary text-xl cursor-pointer min-w-[16px]"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RecentArticles;
