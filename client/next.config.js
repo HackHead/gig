1dad9de85a0b1d9f685b9ea41c84abb82a21a2a6
Andrew <SolomonAndreew@tutanota.com>
2023-05-15 18:16:51 +0300

    style: apply prettier configuration

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        /**
-         * Provide the locales you want to support in your application
-         */
-        locales: ["en", "ru", "ua"],
-        /**
-         * This is the default locale you want to be used when visiting
-         * a non-locale prefixed path.
-         */
-        defaultLocale: "en",
-        localeDetection: true
-    },
-}
+  i18n: {
+    /**
+     * Provide the locales you want to support in your application
+     */
+    locales: ['en', 'ru', 'ua'],
+    /**
+     * This is the default locale you want to be used when visiting
+     * a non-locale prefixed path.
+     */
+    defaultLocale: 'en',
+    localeDetection: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
