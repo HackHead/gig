84a80f3ed837d4f0e128cca14e75ce3ad310e686
Andrew <SolomonAndreew@tutanota.com>
2023-05-23 14:54:58 +0300

    feat: add i18n to the project

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    /**
-     * Provide the locales you want to support in your application
-     */
-    locales: ['en', 'ru', 'ua'],
-    /**
-     * This is the default locale you want to be used when visiting
-     * a non-locale prefixed path.
-     */
-    defaultLocale: 'en',
-    localeDetection: true,
-  },
-};
-
-module.exports = nextConfig;
+const withNextIntl = require('next-intl/plugin')(
+  // This is the default (also the `src` folder is supported out of the box)
+  './i18n.ts'
+);
+ 
+module.exports = withNextIntl({
+  // Other Next.js configuration ...
+  experimental: {appDir: true}
+});
