f4d37832fd6b05237643c96e60bfb03d52452c86
Andrew <SolomonAndreew@tutanota.com>
2023-05-27 20:45:11 +0300

    style: update spinner styles with clsx and tailwind instead of .scss

--- client/components/atoms/Spinner/index.tsx
+++ client/components/atoms/Spinner/index.tsx
@@ -1,10 +1,18 @@
+import clsx from "clsx";
+
 export interface SpinnerProps {
     size?: 'small' | 'medium' | 'large'
 }
 
 const Spinner = ({size = 'small'}: SpinnerProps) => {
+    const spinnerClass = clsx(
+        "spinner",
+        { "h-[40px] w-[40px] border-[4px]": size === "large" },
+        { "h-[30px] w-[30px] border-[3px]": size === "medium" },
+        { "h-[20px] w-[20px] border-[2px]": size === "small" }
+      )
     return (
-        <div className={`spinner ${size}`}></div>
+        <div className={spinnerClass}></div>
     );
 };
 
